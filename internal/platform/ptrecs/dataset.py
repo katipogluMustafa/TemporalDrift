28e88dd88e5ef13e1367db6143c76aea87ee3d6a
Yukawa <mustafakatipoglu98@hotmail.com>
2020-04-10 12:47:49 +0300

    split movielensdataset.load()

--- internal/platform/ptrecs/dataset.py
+++ internal/platform/ptrecs/dataset.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import pandas as pd
+from datetime import datetime
 import os
 
 
@@ -12,8 +13,53 @@ class Dataset(ABC):
 
 
 class MovieLensDataset(Dataset):
-    def __init__(self):
+    def __init__(self,
+                 ratings_col_names=('user_id', 'item_id', 'rating', 'timestamp'),
+                 ratings_path=r'C:\Users\Yukawa\datasets\ml-latest-small\ratings.csv',
+                 movies_col_names=('item_id', 'title', 'genres'),
+                 movies_path=r'C:\Users\Yukawa\datasets\ml-latest-small\movies.csv',
+                 is_ratings_cached=True,
+                 is_movies_cached=True):
         Dataset.__init__(self)
+        self.is_ratings_cached = is_ratings_cached
+        self.is_movies_cached = is_movies_cached
+        self.ratings = MovieLensDataset.load_ratings(ratings_path,
+                                                     ratings_col_names) if self.is_ratings_cached else None
+        self.movies = MovieLensDataset.load_movies(movies_path,
+                                                   movies_col_names) if self.is_movies_cached else None
+
+    @staticmethod
+    def load_movies(movies_path,
+                    movies_col_names=('item_id', 'title', 'genres')):
+        if not os.path.isfile(movies_path) or not movies_col_names:
+            return None
+
+        # read movies
+        movies = pd.read_csv(movies_path, sep=',', header=1, names=movies_col_names)
+
+        # Extract Movie Year
+        movies['year'] = movies.title.str.extract("\((\d{4})\)", expand=True)
+        movies.year = pd.to_datetime(movies.year, format='%Y')
+        movies.year = movies.year.dt.year  # As there are some NaN years, resulting type will be float (decimals)
+
+        # Remove year part from the title
+        movies.title = movies.title.str[:-7]
+
+        return movies
+
+    @staticmethod
+    def load_ratings(ratings_path,
+                     ratings_col_names=('user_id', 'item_id', 'rating', 'timestamp')):
+        if not os.path.isfile(ratings_path) or not ratings_col_names:
+            return None
+
+        # read ratings
+        ratings = pd.read_csv(ratings_path, sep=',', header=1, names=ratings_col_names)
+
+        # Convert timestamp into readable format
+        ratings['timestamp'] = pd.to_datetime(ratings['timestamp'], unit='s', origin='unix')
+
+        return ratings
 
     @staticmethod
     def load(ratings_col_names=('user_id', 'item_id', 'rating', 'timestamp'),
@@ -21,27 +67,10 @@ class MovieLensDataset(Dataset):
              movies_col_names=('item_id', 'title', 'genres'),
              movies_path=r'C:\Users\Yukawa\datasets\ml-latest-small\movies.csv'
              ):
-
-        # Check if given paths are valid
-        if not os.path.isfile(ratings_path) or not os.path.isfile(movies_path):
-            return None
-
-        # Check given column names are not empty
-        if not ratings_col_names or not movies_col_names:
-            return None
-
-        # Read the files
-        ratings = pd.read_csv(ratings_path, sep=',', header=1, names=ratings_col_names)
-        movies = pd.read_csv(movies_path, sep=',', header=0, names=movies_col_names)
-
-        # Extract Movie Year
-        movies['year'] = movies.title.str.extract("\((\d{4})\)", expand=True)
-        movies.year = pd.to_datetime(movies.year, format='%Y')
-        movies.year = movies.year.dt.year  # As there are some NaN years, resulting type will be float (decimals)
-        # Remove year part from the title
-        movies.title = movies.title.str[:-7]
-        # Convert timestamp into readable format
-        ratings['timestamp'] = pd.to_datetime(ratings['timestamp'], unit='s', origin='unix')
+        # Load movies
+        movies = MovieLensDataset.load_movies(movies_path=movies_path, movies_col_names=movies_col_names)
+        # Load ratings
+        ratings = MovieLensDataset.load_ratings(ratings_path=ratings_path, ratings_col_names=ratings_col_names)
 
         # Merge the ratings and movies
         movie_ratings = pd.merge(ratings, movies, on='item_id')
