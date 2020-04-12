f3fbaa5b1470ad857f171feb88f6cf087a02f433
Yukawa <mustafakatipoglu98@hotmail.com>
2020-04-12 16:32:26 +0300

    Clear completed TODOs from comments

--- internal/platform/ptmrecs/trainset.py
+++ internal/platform/ptmrecs/trainset.py
@@ -64,7 +64,6 @@ class TrainsetUser:
 
         return random.choices(population=self.get_users(), k=n)
 
-    # TODO: NAME CHANGED
     def get_user_ratings(self, user_id: int) -> pd.DataFrame:
         """
         Get all the ratings given by of the chosen users
@@ -79,7 +78,6 @@ class TrainsetUser:
 
         return data.loc[data['user_id'] == user_id]
 
-    # TODO: NAME CHANGED
     def get_user_avg(self, user_id: int):
 
         if self.cache.use_avg_ratings_cache:
@@ -89,7 +87,6 @@ class TrainsetUser:
         user_ratings = self.get_user_ratings(user_id=user_id)
         return user_ratings.rating.mean() if not user_ratings.empty else 0
 
-    # TODO: NAME CHANGED
     def get_timestamp(self, user_id: int, movie_id: int):
         """
         Get the timestamp of the given rating
@@ -118,7 +115,7 @@ class TrainsetUser:
         user_ratings = self.get_user_ratings(user_id=user_id)
         return user_ratings.timestamp.mean() if not user_ratings.empty else 0
 
-    # TODO: Put into TemporalDatasetUser
+    # TODO: Later, create TemporalDatasetUser, and put this method into that one
     def get_user_ratings_at(self, user_id: int, at: datetime) -> pd.DataFrame:
         """
         Get user ratings up until the given datetime
@@ -134,8 +131,7 @@ class TrainsetUser:
 
         return data.loc[(data['user_id'] == user_id) & (data.timestamp < at)]
 
-    # TODO: NAME CHANGED
-    # TODO: Put into TemporalDatasetUser
+    # TODO: Later, create TemporalDatasetUser, and put this method into that one
     def get_user_avg_at(self, user_id: int, at: datetime):
         user_ratings = self.get_user_ratings_at(user_id, at)
         return user_ratings.rating.mean() if not user_ratings.empty else 0
@@ -162,7 +158,6 @@ class TrainsetMovie:
             return self.cache.movies.loc[self.cache.movies['item_id'] == movie_id]
         return self.cache.movie_ratings.loc[self.cache.movie_ratings['item_id'] == movie_id]
 
-    # TODO: NAME CHANGED
     def get_movies(self):
         """
         Get list of unique 'item_id's or in other words the movies.
@@ -207,7 +202,6 @@ class TrainsetMovie:
                                      & (movie_ratings.timestamp < time_constraint.end_dt)][['item_id', 'rating']]
         raise Exception("Undefined time_constraint is given!")
 
-    # TODO: NAME CHANGED
     def get_movie_rating(self, movie_id: int, user_id: int) -> int:
         """
         Get the movie rating taken by the chosen user
@@ -236,7 +230,6 @@ class TrainsetMovie:
         movies_watched = self.get_movies_watched(user_id=user_id)
         return random.choice(movies_watched['item_id'].values.tolist()) if not movies_watched.empty else 0
 
-    # TODO: NAME CHANGED
     def get_random_movies_watched(self, user_id: int, n=2) -> pd.DataFrame:
         """
         Get random n movies watched by the user. Only use when n > 2
