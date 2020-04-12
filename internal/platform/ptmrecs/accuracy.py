a78987c9e5608f6bc6053242ab4eb35935a5bc0e
Yukawa <mustafakatipoglu98@hotmail.com>
2020-04-12 11:29:06 +0300

    Refactor Accuracy.rmse, do not count zero predictions

--- internal/platform/ptmrecs/accuracy.py
+++ internal/platform/ptmrecs/accuracy.py
@@ -6,20 +6,23 @@ class Accuracy:
     @staticmethod
     def rmse(predictions) -> float:
         if type(predictions) is pd.DataFrame:
-            number_of_predictions = predictions.count()
+            number_of_predictions = 0
             sum_of_square_differences = 0.0
             for row in predictions.itertuples(index=False):
-                prediction_rating = row[0]   # row[0] : prediction, row[1] : actual rating
-                if prediction_rating != 0:
-                    sum_of_square_differences += (row[1] - prediction_rating) ** 2
-            result = sum_of_square_differences / number_of_predictions
-            return result[0]
+                # row[1] : actual rating, row[0] : prediction
+                prediction = row[0]
+                if prediction != 0:
+                    sum_of_square_differences += (row[1] - prediction) ** 2
+                    number_of_predictions += 1
+            return sum_of_square_differences / number_of_predictions if number_of_predictions != 0 else 0
         elif type(predictions) is list:
-            number_of_predictions = len(predictions)
+            number_of_predictions = 0
             sum_of_square_differences = 0.0
             for prediction, actual in predictions:
-                sum_of_square_differences += (actual - prediction) ** 2
-            result = sum_of_square_differences / number_of_predictions
-            return result
+                if prediction != 0:
+                    sum_of_square_differences += (actual - prediction) ** 2
+                    number_of_predictions += 1
+            return sum_of_square_differences / number_of_predictions if number_of_predictions != 0 else 0
+
 
 
